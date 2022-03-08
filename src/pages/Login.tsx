8b253a9eae516566d304569d9a7235996584dd7e
Pavelnagai <pavel.nagai@mail.ru>
2022-03-08 23:08:22 +0300

    login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,65 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import {useDispatch, useSelector} from "react-redux";
+import {ErrorMessage, Field, Form, Formik} from "formik";
+import {authTC} from "../redux/reducers/authReducer";
+import {rootReducerType} from "../redux/store";
+import {useNavigate} from "react-router-dom";
 
 type propsLoginType = {}
 
 const Login = (props: propsLoginType) => {
+    const auth = useSelector<any, rootReducerType>(state => state.auth.isAuth)
+    const navigate = useNavigate()
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const dispatch = useDispatch()
+    useEffect(() => {
+        if (!auth){
+            dispatch(authTC(email, password))
+        }else {
+            navigate('/profile')
+        }
+
+    }, [email, password, auth])
+
     return (
         <div>
-            Login
+            <h2>Login</h2>
+            <Formik
+                initialValues={{email: '', password: ''}}
+                validate={values => {
+                    const errors:any = {};
+                    if (!values.email) {
+                        errors.email = 'Required';
+                    } else if (
+                        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                    ) {
+                         errors.email = 'Invalid email address';
+                    }
+                    return errors;
+                }}
+                onSubmit={(values, {setSubmitting}) => {
+                    setEmail(values.email)
+                    setPassword(values.password)
+                    // navigate('/profile')
+                }}
+            >
+                {({isSubmitting}) => (
+                    <Form>
+                        <h5>Email</h5>
+                        <Field type="email" name="email"/>
+                        <ErrorMessage name="email" component="div"/>
+                        <h5>Password</h5>
+                        <Field type="password" name="password"/>
+                        <ErrorMessage name="password" component="div"/>
+                        <div>
+                            <button type="submit" disabled={isSubmitting}>
+                                login
+                            </button>
+                        </div>
+                    </Form>
+                )}
+            </Formik>
         </div>
     )
 }
